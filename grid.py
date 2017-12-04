e0b6b352023bfc9de3d92d0af76a9a3c7cfb69af
Andrew <saintruler@gmail.com>
2017-12-04 14:47:58 +0400

    Fixed grid and added some functionality.

--- grid.py
+++ grid.py
@@ -4,13 +4,15 @@ import sys
 
 class Grid:
     def __init__(self, surface):
+        self.resized = False
         self.surface = surface
 
-        self.center_x = 0
-        self.center_y = 0
+        self.current_x = 0
+        self.current_y = 0
 
         self.cell_size = 50
-        self.nlines = 30
+        self.xnlines = (self.surface.get_rect().width // self.cell_size) * 2
+        self.ynlines = (self.surface.get_rect().height // self.cell_size) * 2
 
         self.font_size = 18
         self.font = pygame.font.SysFont('couriernew', self.font_size, bold=True)
@@ -20,6 +22,16 @@ class Grid:
 
     def new_vert_line(self, x_pos, width=1):
         pygame.draw.line(self.surface, BLACK, (x_pos, 0), (x_pos, h), width)
+
+    def new_hor_num(self, y_pos, num):
+        text = self.font.render(str(num), 3, BLACK)
+        rect = text.get_rect(x=5, centery=y_pos)
+
+        bg = pygame.Surface(rect.size)
+        bg.fill(GRAY)
+
+        self.surface.blit(bg, rect)
+        self.surface.blit(text, rect)
 
     def new_vert_num(self, x_pos, num):
         text = self.font.render(str(num), 3, BLACK)
@@ -32,35 +44,32 @@ class Grid:
         self.surface.blit(bg, rect)
         self.surface.blit(text, rect)
 
-    def new_hor_num(self, y_pos, num):
-        text = self.font.render(str(num), 3, BLACK)
-        rect = text.get_rect(x=5, centery=y_pos)
-
-        bg = pygame.Surface(rect.size)
-        bg.fill(GRAY)
-
-        self.surface.blit(bg, rect)
-        self.surface.blit(text, rect)
+    def update(self):
+        self.xnlines = (self.surface.get_rect().width // self.cell_size) * 2
+        self.ynlines = (self.surface.get_rect().height // self.cell_size) * 2
+        self.resized = False
 
     def draw(self):
-        x = self.center_x // self.cell_size
-        y = self.center_y // self.cell_size
-        for i in range(self.nlines):
-            x_pos = int(self.center_x) - x * self.cell_size + self.cell_size * i
-            width = 5 if x - i == 0 else 1
-            self.new_hor_line(x_pos, width)
+        x = self.current_x // self.cell_size
+        y = self.current_y // self.cell_size
 
-        for i in range(self.nlines):
-            y_pos = int(self.center_y) - y * self.cell_size + self.cell_size * i
-            width = 5 if y - i == 0 else 1
-            self.new_vert_line(y_pos, width)
+        for _ in range(-3, self.xnlines - 3):
+            x_pos = int(self.current_x) - x * self.cell_size + self.cell_size * _
+            width = 5 if x - _ == 0 else 1
+            self.new_vert_line(x_pos, width)
 
-        for i in range(self.nlines):
-            x_pos = int(self.center_x) - x * self.cell_size + self.cell_size * i
-            y_pos = int(self.center_y) - y * self.cell_size + self.cell_size * i
+        for _ in range(self.ynlines):
+            y_pos = int(self.current_y) - y * self.cell_size + self.cell_size * _
+            width = 5 if y - _ == 0 else 1
+            self.new_hor_line(y_pos, width)
 
-            self.new_hor_num(x_pos, x - i)
-            self.new_vert_num(y_pos, y - i)
+        for _ in range(-3, self.xnlines - 3):
+            x_pos = int(self.current_x) - x * self.cell_size + self.cell_size * _
+            self.new_vert_num(x_pos, -(x - _))
+
+        for _ in range(-3, self.ynlines - 3):
+            y_pos = int(self.current_y) - y * self.cell_size + self.cell_size * _
+            self.new_hor_num(y_pos, y - _)
 
     def resize(self, delta):
         if delta < 0:
@@ -71,7 +80,8 @@ class Grid:
 
                 self.cell_size += 10
                 if self.font_size < 18:
-                    self.nlines -= 15
+                    self.xnlines -= 15
+                    self.ynlines -= 15
         else:
             # scale down
             if self.cell_size - 10 > 20:
@@ -79,11 +89,21 @@ class Grid:
                 self.font = pygame.font.SysFont('couriernew', self.font_size, bold=True)
 
                 self.cell_size -= 10
-                self.nlines += 15
+                self.xnlines += 15
+                self.ynlines += 15
 
-    def move(self, delta_y, delta_x):
-        self.center_x += delta_x
-        self.center_y += delta_y
+    def move(self, delta_x, delta_y):
+        self.current_x += delta_x
+        self.current_y += delta_y
+
+    def apply_event(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button in (4, 5) and not self.resized:
+                self.resize(event.button - 5)
+        elif event.type == pygame.MOUSEMOTION and pygame.mouse.get_pressed()[0]:
+            self.move(*event.rel)
+        elif event.type == pygame.VIDEORESIZE:
+            self.resized = True
 
 
 BLACK = (0, 0, 0)
@@ -91,7 +111,7 @@ GRAY = (136, 136, 136)
 
 pygame.init()
 w, h = size = 1280, 720
-screen = pygame.display.set_mode(size)
+screen = pygame.display.set_mode(size, pygame.RESIZABLE)
 
 grid = Grid(screen)
 
@@ -101,12 +121,12 @@ while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button in (4, 5):
-                grid.resize(event.button - 5)
-        elif event.type == pygame.MOUSEMOTION and pygame.mouse.get_pressed()[0]:
-            grid.move(*event.rel)
+        elif event.type == pygame.VIDEORESIZE:
+            w, h = size = event.size
+            screen = pygame.display.set_mode(size, pygame.RESIZABLE)
+        grid.apply_event(event)
 
     screen.fill(GRAY)
+    grid.update()
     grid.draw()
     pygame.display.flip()
