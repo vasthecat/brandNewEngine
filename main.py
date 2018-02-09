0d138497db7bf3cbd0d1f56148b989d5b5fd50f8
Andrew <saintruler@gmail.com>
2018-02-09 20:45:56 +0400

    A

--- main.py
+++ main.py
@@ -5,29 +5,38 @@ import sys
 
 from engine.game_objects import GameObject
 from engine.base_components import ImageComponent
-from user_components import ControllerComponent, ShooterComponent
+from user_components import PhysicsCollider, TriggerCollider, PlayerController
 from engine.scene_manager import scene_manager
 from engine.input_manager import input_manager
 
-scene_manager.rename_scene('default_scene', 'scene1')
 scene = scene_manager.current_scene
-input_manager.add_axis('Rotation', {
-    pygame.K_q: -1,
-    pygame.K_e: 1,
-})
+
+bg = GameObject()
+bg.add_component(ImageComponent('images/ground.png', bg))
+scene.add_object(bg)
+
+house1 = GameObject(-1428, 1123)
+house1.add_component(ImageComponent('images/house.png', house1))
+house1.add_component(PhysicsCollider(
+    0, 0,
+    house1.get_component(ImageComponent).image.get_rect(),
+    house1
+))
+scene.add_object(house1)
+
+player = GameObject()
+player.add_component(ImageComponent('images/player.png', player))
+player.add_component(PhysicsCollider(0, -64, [0, 0, 64, 20], player))
+player.add_component(TriggerCollider(64, -32, [0, 0, 64, 128], player))
+player.add_component(PlayerController(10, player))
+scene.add_object(player)
+
+house2 = GameObject(-1500, -500)
+house2.add_component(ImageComponent('images/house_back.png', house2))
+scene.add_object(house2)
 
 obj = GameObject()
-obj.add_component(ControllerComponent(15, obj))
-obj.add_component(ShooterComponent(50, 3, 0.15, obj))
-obj.add_component(ImageComponent('images/player.png', obj))
-
-for y in range(-8, 8):
-    for x in range(-8, 8):
-        bg = GameObject(x * 512, y * 512)
-        bg.add_component(ImageComponent('images/bg.png', bg))
-
-        scene.add_object(bg)
-
+obj.add_component(TriggerCollider(0, 0, [0, 0, 100, 100], obj))
 scene.add_object(obj)
 
 clock = pygame.time.Clock()
--- user_components.py
+++ user_components.py
@@ -1,85 +1,66 @@
 from engine.input_manager import input_manager
 from engine.scene_manager import scene_manager
-from engine.base_components import Component, TransformComponent, ImageComponent
-from engine.game_objects import GameObject
+from engine.base_components import Component
 
 from pygame.math import Vector2
 import pygame
 
-from math import acos, degrees
-from time import time
 
-
-class ControllerComponent(Component):
+class PlayerController(Component):
     def __init__(self, speed, game_object):
         super().__init__(game_object)
-        self.transform = self.game_object.get_component(TransformComponent)
         self.speed = speed
-        self.sound = pygame.mixer.Sound('sounds/steps.ogg')
-        self._prev_move = Vector2()
-
-    def get_mouse_coord(self):
-        mouse = input_manager.get_mouse_pos()
-        cam = scene_manager.current_scene.current_camera
-        t = cam.transform
-        mouse_x = t.x + (mouse[0] - cam.surface.get_rect().center[0])
-        mouse_y = t.y + (cam.surface.get_rect().center[1] - mouse[1])
-        return mouse_x, mouse_y
 
     def update(self, *args):
         hor = input_manager.get_axis('Horizontal')
         vert = input_manager.get_axis('Vertical')
-        mouse = self.get_mouse_coord()
 
-        delta = Vector2(mouse[0] - self.transform.x, mouse[1] - self.transform.y)
-        try:
-            cos = delta.x / delta.length()
-            sin = delta.y / delta.length()
-            rot = degrees(acos(cos))
-            if sin < 0:
-                rot = -rot
-        except:
-            rot = 0
+        x, y = self.game_object.transform.coord
 
-        move = Vector2(hor, vert)
-        if move.length() != 0:
-            move = move.normalize() * self.speed
-            if self._prev_move.length() == 0:
-                pygame.mixer.Channel(0).play(self.sound, -1)
-        else:
-            pygame.mixer.Channel(0).stop()
-        self._prev_move = move
+        cam_move = Vector2(
+            hor * self.speed if abs(x + hor * self.speed) < 1370 else 0,
+            vert * self.speed if abs(y + vert * self.speed) < 1650 else 0
+        )
+        scene_manager.current_scene.current_camera.transform.move(cam_move.x, cam_move.y)
 
-        self.transform.move(move.x, move.y)
-        self.transform.set_rotation(rot)
-        scene_manager.current_scene.current_camera.transform.move(move.x, move.y)
+        move = Vector2(hor * self.speed, vert * self.speed)
+        self.game_object.transform.move(move.x, move.y)
+
+        for obj in scene_manager.current_scene.objects:
+            phys_collider = self.game_object.get_component(PhysicsCollider)
+            trigger_collider = self.game_object.get_component(TriggerCollider)
+
+            if phys_collider is not None:
+                phys_collider.update()
+                if obj != self.game_object and obj.has_component(PhysicsCollider):
+                    if phys_collider.detect_collision(obj.get_component(PhysicsCollider)):
+                        scene_manager.current_scene.current_camera.transform.move(-cam_move.x, -cam_move.y)
+                        self.game_object.transform.move(-move.x, -move.y)
+            if trigger_collider is not None:
+                trigger_collider.update()
+                if obj != self.game_object and obj.has_component(TriggerCollider):
+                    if trigger_collider.detect_collision(obj.get_component(TriggerCollider)):
+                        print('trigger') # TODO
 
 
-class ShooterComponent(Component):
-    def __init__(self, speed, life_time, rate_of_fire, game_object):
+class Collider(Component):
+    def __init__(self, shift_x, shift_y, rect, game_object):
         super().__init__(game_object)
-        self.speed = speed
-        self.life_time = life_time
-        self.rate_of_fire = rate_of_fire
-        self.bullets = []
-        self.prev_t = -1
-        self.scene = scene_manager.current_scene
-        self.sound = pygame.mixer.Sound('sounds/shot.ogg')
+        self.shift_x = shift_x
+        self.shift_y = shift_y
+        self.rect = pygame.Rect(rect)
+
+    def detect_collision(self, collider):
+        return self.rect.colliderect(collider.rect)
 
     def update(self, *args):
-        if pygame.mouse.get_pressed()[0] and time() - self.prev_t >= self.rate_of_fire:
-            pygame.mixer.Channel(1).play(self.sound)
-            bullet = GameObject(*self.game_object.transform.coord)
-            bullet.add_component(ImageComponent('images/bullet.png', bullet))
-            bullet.transform.set_rotation(self.game_object.transform.rotation)
-            self.bullets.append((bullet, time()))
-            self.scene.add_object(bullet)
-            self.prev_t = time()
+        self.rect.centerx = self.game_object.transform.x + self.shift_x
+        self.rect.centery = self.game_object.transform.y + self.shift_y
 
-        for bullet, t in self.bullets:
-            if time() - t >= self.life_time:
-                self.scene.remove_object(bullet)
-                self.bullets.remove((bullet, t))
-            else:
-                move = Vector2(1, 0).rotate(bullet.transform.rotation).normalize() * self.speed
-                bullet.transform.move(move.x, move.y)
+
+class PhysicsCollider(Collider):
+    pass
+
+
+class TriggerCollider(Collider):
+    pass
