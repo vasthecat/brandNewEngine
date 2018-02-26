e364e66536b64e5923ce81a3a04fcbac9a5a372b
Andrew <saintruler@gmail.com>
2018-02-26 16:55:44 +0400

    Added SaveManager Engine initialize parameters now stored in config.json file
    Signed-off-by: Andrew <saintruler@gmail.com>

--- engine/initialize_engine.py
+++ engine/initialize_engine.py
@@ -1,6 +1,9 @@
 import pygame
+from engine.save_manager import SaveManager
+
+config = SaveManager.load_profile('config', 'config.json')
 
 pygame.init()
-pygame.display.set_caption("Untitled Game")
-width, height = resolution = 1280, 720
-pygame.display.set_mode(resolution)
+pygame.display.set_caption(config.get('title'))
+width, height = resolution = config.get('resolution')
+pygame.display.set_mode(resolution, pygame.FULLSCREEN if config.get('fullscreen') else 0)
---
+++ engine/save_manager.py
@@ -0,0 +1,38 @@
+import json
+
+
+class SaveManager:
+    profiles = {}
+
+    @staticmethod
+    def add_profile(name, init_dict=None):
+        SaveManager.profiles[name] = {} if init_dict is None else init_dict
+        return SaveManager.profiles[name]
+
+    @staticmethod
+    def remove_profile(name):
+        del SaveManager.profiles[name]
+
+    @staticmethod
+    def save_profile(name, path):
+        with open(path, 'w', encoding='utf-8') as f:
+            json.dump(f, SaveManager.profiles.get(name))
+
+    @staticmethod
+    def load_profile(name, path):
+        with open(path, encoding='utf-8') as f:
+            SaveManager.profiles[name] = json.load(f)
+        return SaveManager.profiles[name]
+
+    @staticmethod
+    def add_entry(profile_name, entry_name, value):
+        SaveManager.profiles[profile_name][entry_name] = value
+        return value
+
+    @staticmethod
+    def get_entry(profile_name, entry_name):
+        return SaveManager.profiles[profile_name][entry_name]
+
+    @staticmethod
+    def remove_entry(profile_name, entry_name):
+        del SaveManager.profiles[profile_name][entry_name]
